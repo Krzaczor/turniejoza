b459bf7663d9011fea83d8156bd43ddc64215ef5
HurasAdam <huradam@wp.pl>
2025-07-28 15:27:59 +0200

    feat:add dummy categories and questions data.

---
+++ src/renderer/src/constants/index.ts
@@ -0,0 +1,4 @@
+import { questions } from "./mocked-questions";
+import { categories } from "./mocked-categories";
+
+export const mockData = {questions,categories}
---
+++ src/renderer/src/constants/mocked-categories.ts
@@ -0,0 +1,12 @@
+export const categories: Category[] = [
+  { id: 'cat-1', name: 'Programowanie' },
+  { id: 'cat-2', name: 'Matematyka' },
+  { id: 'cat-3', name: 'Geografia' },
+  { id: 'cat-4', name: 'Absurdalne Fakty' },
+  { id: 'cat-5', name: 'Życie Programisty' },
+  { id: 'cat-6', name: 'Teorie Spiskowe' },
+  { id: 'cat-7', name: 'Kuchnia dla leniwych' },
+  { id: 'cat-8', name: 'Prawda o pracy zdalnej' },
+  { id: 'cat-9', name: 'Memologia' },
+  { id: 'cat-10', name: 'Coś, czego nie uczą w szkole' }
+];
